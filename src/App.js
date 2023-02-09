dd9dfaed8a6a00dd7f52bfd3941f9f71e19bdcc2
Sam Harris <samharrisc@gmail.com>
2023-02-08 16:00:50 -0800

    Added form

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { GameListPage } from './features/games/GameListPage';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
 import './App.css'
+import { useFormikContext } from 'formik';
 
 export default function App() {
   return (
--- src/features/games/AddGame.js
+++ src/features/games/AddGame.js
@@ -1,12 +1,48 @@
-import { Form, FormGroup } from "reactstrap"
+import { Formik, Field, Form, formGroup } from "formik";
+import { useState } from "react";
+import { useDispatch } from "react-redux";
+import { Button, FormGroup, Label, Modal, ModalBody, ModalHeader } from "reactstrap";
 
 const AddGame = () => {
+    const [modalOpen, setModalOpen] = useState(false);
+
+    const dispatch = useDispatch();
+
+    const handleSubmit = (values) => {
+        const game = {
+            gameTitle: values.gameTitle,
+            platform: values.platform
+        }
+        console.log(game);
+        dispatch()
+    }
+
     return (
-        <div>
-            <Form>
-                <FormGroup></FormGroup>
-            </Form>
-        </div>
+        <>
+            <Button outline color='warning' className="text-dark" onClick={() => setModalOpen(true)}><i className='fa fa-pencil fa-lg' /> Add Game</Button>
+                <Modal isOpen={modalOpen}>
+                    <ModalHeader className='bg-warning' toggle={() => setModalOpen(false)}>Add Game</ModalHeader>
+                        <ModalBody>
+                            <Formik
+                            initialValues={{
+                                gameTitle: '',
+                                platform: ''
+                            }} >
+                                <Form>
+                                    <FormGroup>
+                                        <Label for='gameTitle'>Game Title</Label>
+                                        <Field name='gameTitle' class='formcontrol' placeholder='Game Title' />
+                                    </FormGroup>
+                                    <FormGroup>
+                                        <Label>Platform</Label>
+                                        <Field name='platform' class='formcontrol' placeholder='platform' />
+                                    </FormGroup>
+                                    <Button type='submit' outline className='text-dark' color='warning'>Submit</Button>
+                                </Form>
+                            </Formik>
+                        </ModalBody>
+                </Modal>
+        </>
     )
 }
 
